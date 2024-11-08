57560494262fdb04d3d8d71000adc5058c6bd9cd
Ruben <rubenhaayrapetyan3@gmail.com>
2024-11-08 23:53:51 +0400

    App gradle clean up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.jetbrains.kotlin.android)
-  alias(libs.plugins.compose.compiler)
   alias(libs.plugins.hilt.android)
   alias(libs.plugins.ksp)
 }
@@ -36,9 +35,6 @@ android {
   composeOptions {
     kotlinCompilerExtensionVersion = "1.5.15"
   }
-  buildFeatures {
-    compose = true
-  }
 }
 
 dependencies {
@@ -47,29 +43,8 @@ dependencies {
   implementation(project(":domain"))
   implementation(project(":entity"))
 
-  implementation (libs.jsoup) // TODO
-  implementation (libs.androidx.navigation.compose)
   implementation (libs.retrofit)
   implementation (libs.retrofit2.converter.moshi)
-  implementation (libs.logging.interceptor)
-  implementation(libs.androidx.lifecycle.runtime.ktx.v270)
-  implementation (libs.androidx.lifecycle.viewmodel.ktx)
-  implementation(libs.androidx.activity.compose.v182)
   implementation(libs.hilt.android)
   ksp(libs.hilt.compiler)
-  implementation (libs.hilt.navigation.compose)
-  implementation(libs.coil.compose)
-  implementation(libs.androidx.core.ktx)
-  implementation(platform(libs.androidx.compose.bom))
-  implementation(libs.androidx.ui)
-  implementation(libs.androidx.ui.graphics)
-  implementation(libs.androidx.ui.tooling.preview)
-  implementation(libs.androidx.material3)
-  testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
-  androidTestImplementation(platform(libs.androidx.compose.bom))
-  androidTestImplementation(libs.androidx.ui.test.junit4)
-  debugImplementation(libs.androidx.ui.tooling) // TODO
-  debugImplementation(libs.androidx.ui.test.manifest)
 }
